7e063c8dfb670e787357f7785c19c9af3d8304ff
safwanahmadsaffi <sahmedsaffi@gmail.com>
2025-03-23 19:51:41 +0500

    declaring Parallel Matrix Multiplication Function

--- static.c
+++ static.c
@@ -4,3 +4,14 @@
 #include <omp.h>
 #define N 500  // Matrix size
 #define NUM_THREADS 12  // Adjust based on CPU cores
+void multiply_matrices_parallel(int A[N][N], int B[N][N], int C[N][N]) {
+    #pragma omp parallel for num_threads(NUM_THREADS)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
