091fb05a0a8f3f0fb48dd98e1b3a71ef30d59be1
safwanahmadsaffi <sahmedsaffi@gmail.com>
2025-03-23 20:05:52 +0500

    execution time

--- sequential.c
+++ sequential.c
@@ -12,3 +12,13 @@ void multiply_matrices(int A[N][N], int B[N][N], int C[N][N]) {
         }
     }
 }
+double get_execution_time() {
+    int A[N][N], B[N][N], C[N][N];
+
+    // Initialize matrices with random values
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = rand() % 10;
+            B[i][j] = rand() % 10;
+        }
+    }
