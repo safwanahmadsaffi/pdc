2b9179edf3e50c326008bea4e4152786362345bd
safwanahmadsaffi <sahmedsaffi@gmail.com>
2025-03-23 20:04:53 +0500

    adding Matrix Multiplication Function

--- sequential.c
+++ sequential.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #define N 500  // Matrix size
+void multiply_matrices(int A[N][N], int B[N][N], int C[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
